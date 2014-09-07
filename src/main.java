f46c080d4bdcb3494df989842e53aa5b02eacc90
kyle7273 <marioman861@gmail.com>
2014-09-07 08:36:03 -0700

    Added variables to Ponai to store job ids, job building ids, job room ids, job's name, job position

--- src/main.java
+++ src/main.java
@@ -2,21 +2,21 @@ public class main{
 	public static void main(String[] args){
 		//Todo: Random name generation
 		//Finish writing all 2 million entries.
-		ponai Ponai1 = doLoadPony(Ponai1 = new ponai(),"Twilight","Sparkle",10,"mare");
-		ponai Ponai2 = doLoadPony(Ponai2 = new ponai(),"Rainbow","Dash",10,"mare");
-		ponai Ponai3 = doLoadPony(Ponai3 = new ponai(),"Pinkie","Pie",10,"mare");
-		ponai Ponai4 = doLoadPony(Ponai4 = new ponai(),"Fluttershy","",10,"mare");
-		ponai Ponai5 = doLoadPony(Ponai5 = new ponai(),"Applejack","",10,"mare");
-		ponai Ponai6 = doLoadPony(Ponai6 = new ponai(),"Rarity","",10,"mare");
-		ponai Ponai7 = doLoadPony(Ponai7 = new ponai(),"Sweetie","Belle",5,"mare");
-		ponai Ponai8 = doLoadPony(Ponai8 = new ponai(),"Apple","Bloom",5,"mare");
-		ponai Ponai9 = doLoadPony(Ponai9 = new ponai(),"Scootaloo","",5,"mare");
-		ponai Ponai10 = doLoadPony(Ponai10 = new ponai(),"Carrot","Top",10,"mare");
+		ponai Ponai1 = doLoadPony(Ponai1 = new ponai(),"Twilight","Sparkle",10,"mare",1,1,1,1,"Librarian","Owner");
+		//ponai Ponai2 = doLoadPony(Ponai2 = new ponai(),"Rainbow","Dash",10,"mare");
+		//ponai Ponai3 = doLoadPony(Ponai3 = new ponai(),"Pinkie","Pie",10,"mare");
+		//ponai Ponai4 = doLoadPony(Ponai4 = new ponai(),"Fluttershy","",10,"mare");
+		//ponai Ponai5 = doLoadPony(Ponai5 = new ponai(),"Applejack","",10,"mare");
+		//ponai Ponai6 = doLoadPony(Ponai6 = new ponai(),"Rarity","",10,"mare");
+		//ponai Ponai7 = doLoadPony(Ponai7 = new ponai(),"Sweetie","Belle",5,"mare");
+		//ponai Ponai8 = doLoadPony(Ponai8 = new ponai(),"Apple","Bloom",5,"mare");
+		//ponai Ponai9 = doLoadPony(Ponai9 = new ponai(),"Scootaloo","",5,"mare");
+		//ponai Ponai10 = doLoadPony(Ponai10 = new ponai(),"Carrot","Top",10,"mare");
 	}
 	
-	public static ponai doLoadPony(ponai Ponai, String firstname, String lastname, int age, String gender){
-		Ponai = new ponai(firstname, lastname, age, gender);
-		System.out.println("Loaded Ponai: " + Ponai.getFirstName() + " " + Ponai.getLastName() + ", a " + Ponai.getAge() + " year old " + Ponai.getGender());
+	public static ponai doLoadPony(ponai Ponai, String firstname, String lastname, int age, String gender,int jobID,int jobBuilding,int jobRoom,int jobPositionID,String jobString,String jobPositionString){
+		Ponai = new ponai(firstname, lastname, age, gender,jobID,jobBuilding,jobRoom,jobPositionID,jobString,jobPositionString);
+		System.out.println("Loaded Ponai: " + Ponai.getFirstName() + " " + Ponai.getLastName() + ", a " + Ponai.getAge() + " year old " + Ponai.getGender() + " working at " + Ponai.getJobString() + " as a " + Ponai.getJobPositionString());
 		return Ponai;
 	}
 }
--- src/ponai.java
+++ src/ponai.java
@@ -3,6 +3,12 @@ public class ponai{
 	public String lastname;
 	public int age;
 	public String gender;
+	public int jobID;
+	public int jobBuilding;
+	public int jobRoom;
+	public int jobPositionID;
+	public String jobString;
+	public String jobPositionString;
 	
 	public ponai(){
 		firstname = "Unnamed";
@@ -11,11 +17,17 @@ public class ponai{
 		gender = "mare";
 	}
 	
-	public ponai(String firstname1, String lastname1, int age1, String gender1){
+	public ponai(String firstname1, String lastname1, int age1, String gender1, int jobID1, int jobBuilding1, int jobRoom1, int jobPositionID1, String jobString1, String jobPositionString1){
 		firstname = firstname1;
 		lastname = lastname1;
 		age = age1;
 		gender = gender1;
+		jobID = jobID1;
+		jobBuilding = jobBuilding1;
+		jobRoom = jobRoom1;
+		jobPositionID = jobPositionID;
+		jobString= jobString1;
+		jobPositionString = jobPositionString1;
 	}
 	
 	public String getFirstName(){
@@ -32,5 +44,25 @@ public class ponai{
 	
 	public String getGender(){
 		return gender;
+	}
+	
+	public int getJobID(){
+		return jobID;
+	}
+	
+	public int getJobBuilding(){
+		return jobBuilding;
+	}
+	
+	public int getJobPositionID(){
+		return jobPositionID;
+	}
+	
+	public String getJobString(){
+		return jobString;
+	}
+	
+	public String getJobPositionString(){
+		return jobPositionString;
 	}
 }
