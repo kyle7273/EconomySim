b6485040a9cc951a5b456b98865170e9348559c4
kyle7273 <marioman861@gmail.com>
2014-09-07 21:27:03 -0700

    Ponai count increased to 20. All randomized.

--- src/main.java
+++ src/main.java
@@ -3,6 +3,25 @@ public class main{
 		//Todo: Finish writing all 2 million entries.
 		//Warning: Wall of laggy text.
 		ponai Ponai1 = doLoadPonai2(Ponai1 = new ponai());
+		ponai Ponai2 = doLoadPonai2(Ponai2 = new ponai());
+		ponai Ponai3 = doLoadPonai2(Ponai3 = new ponai());
+		ponai Ponai4 = doLoadPonai2(Ponai4 = new ponai());
+		ponai Ponai5 = doLoadPonai2(Ponai5 = new ponai());
+		ponai Ponai6 = doLoadPonai2(Ponai6 = new ponai());
+		ponai Ponai7 = doLoadPonai2(Ponai7 = new ponai());
+		ponai Ponai8 = doLoadPonai2(Ponai8 = new ponai());
+		ponai Ponai9 = doLoadPonai2(Ponai9 = new ponai());
+		ponai Ponai10 = doLoadPonai2(Ponai10 = new ponai());
+		ponai Ponai11 = doLoadPonai2(Ponai11 = new ponai());
+		ponai Ponai12 = doLoadPonai2(Ponai12 = new ponai());
+		ponai Ponai13 = doLoadPonai2(Ponai13 = new ponai());
+		ponai Ponai14 = doLoadPonai2(Ponai14 = new ponai());
+		ponai Ponai15 = doLoadPonai2(Ponai15 = new ponai());
+		ponai Ponai16 = doLoadPonai2(Ponai16 = new ponai());
+		ponai Ponai17 = doLoadPonai2(Ponai17 = new ponai());
+		ponai Ponai18 = doLoadPonai2(Ponai18 = new ponai());
+		ponai Ponai19 = doLoadPonai2(Ponai19 = new ponai());
+		ponai Ponai20 = doLoadPonai2(Ponai20 = new ponai());
 	}
 	
 	public static ponai doLoadPony(ponai Ponai, String firstname, String lastname, int age, String gender,int jobID,int jobBuilding,int jobRoom,int jobPositionID,String jobString,String jobPositionString){
