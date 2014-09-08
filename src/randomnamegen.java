33f0cb5fa36b996205b51918b0117e61b093b88c
kyle7273 <marioman861@gmail.com>
2014-09-07 20:28:57 -0700

    Imported list of first names. Todo: Last names.

--- src/randomnamegen.java
+++ src/randomnamegen.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class randomnamegen{
-	static String[] firstnames = {"Bill","Joe"};
+	static String[] firstnames = {"Spicy","Alpha","Danger","Sesame","Brown","Kocher","Mirror","Trick","Star","Vanilla","Banana","Toota","Lion","Water","Furious","Jet","Clover","Dusty","Poppy","Ham","Auto","Corn"," Toga","Hadron","Snake","Lucky","Old","Rug","Shelf","Jasmin","Riiiiiiiiiiiiidge","Bobbing","Crusty","Foxy","Smooth","Clever","Seal","Fuge","BeeBee","Rambli'","Sweet","Silver","Beer","Uncle","Hollow","Water","Miss","Burnt","Spring","Mick","Spring","Snow","Loose","Sun","Finger","Rubber","Blue","Free","Chipoff","Shoe","Laser","Wacky","Common","Fresh","Razor","Citrus","Bravo","Tongue","Sci","High","Window","Dead","Under","Broken","Man","Shell","Whiplash","Glass","Honeycomb","Calm","Air","Mosh","Dragon","Telltale","Eskimo","Leather","Thunder","Wonderstrike","Rotten","Sappy","Swan.Lava","Trick","Honey","Wild"};
 	
 	public static String randomFirstName(){
 		return firstnames[new Random().nextInt(firstnames.length)];
