9aa5d7ed7d9b6ec84484d22824510f398ed4875f
kyle7273 <marioman861@gmail.com>
2014-09-21 02:07:53 -0700

    Moved crap around.

--- src/cmc.java
+++ src/cmc.java
@@ -1,4 +1,10 @@
 public class cmc{
 	//THIS CODE IS FOR RANDOM CRAP TO GO.
 	//THIS IS TOTALLY NOT A REFERENCE TO MY LITTLE PONY... NOPE!
+	
+	public static ponai doLoadPony(ponai Ponai, String firstname, String lastname, int age, String gender,int jobID,int jobBuilding,int jobRoom,int jobPositionID,String jobString,String jobPositionString){
+		Ponai = new ponai(firstname, lastname, age, gender,jobID,jobBuilding,jobRoom,jobPositionID,jobString,jobPositionString);
+		System.out.println("Loaded Ponai: " + Ponai.getFirstName() + " " + Ponai.getLastName() + ", a " + Ponai.getAge() + " year old " + Ponai.getGender() + " working at " + Ponai.getJobString() + " as a " + Ponai.getJobPositionString());
+		return Ponai;
+	}
 }
--- src/main.java
+++ src/main.java
@@ -6,18 +6,6 @@ public class main{
 		ponai[] PonaiArray = doponaiarray(PonaiStoreArrayTemp);
 	}
 	
-	public static ponai doLoadPony(ponai Ponai, String firstname, String lastname, int age, String gender,int jobID,int jobBuilding,int jobRoom,int jobPositionID,String jobString,String jobPositionString){
-		Ponai = new ponai(firstname, lastname, age, gender,jobID,jobBuilding,jobRoom,jobPositionID,jobString,jobPositionString);
-		System.out.println("Loaded Ponai: " + Ponai.getFirstName() + " " + Ponai.getLastName() + ", a " + Ponai.getAge() + " year old " + Ponai.getGender() + " working at " + Ponai.getJobString() + " as a " + Ponai.getJobPositionString());
-		return Ponai;
-	}
-	
-	public static ponai doLoadPonai2(ponai Ponai){
-		int randjobid = randomnamegen.randomJobID();
-		Ponai = doLoadPony(Ponai = new ponai(),randomnamegen.randomFirstName(),randomnamegen.randomLastName(),10,"mare",randjobid,1,1,1,randomnamegen.getJobString(randjobid),"Owner");
-		return Ponai;
-	}
-	
 	public static ponai[] doponaiarray(ponai[] PonaiArray){
 		ponai[] PonaiTempArray = new ponai[PonaiArray.length];
 		for (int i = 0; i < PonaiArray.length; ++i){
